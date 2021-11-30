45bb2c1f98dfa9a8be34b677a672402d2119650d
leaburriat <leaburriat@192.168.115.228>
2021-11-30 10:48:14 +0100

    depotmaster

---
+++ src/mastermind_sf_lb/Mastermind_SF_LB.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mastermind_sf_lb;
+
+/**
+ *
+ * @author leaburriat
+ */
+public class Mastermind_SF_LB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
